2173e87602067bcf4717a0372f68d7d69e3f291e
patataletal <salvadorruelas061@gmail.com>
2025-11-23 07:47:31 -0500

    agrar datos y creacion de tablas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Marca;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+          Marca::factory(30)->create();
+    
+        //User::factory()->create([
+        //   'name' => 'Test User',
+        //    'email' => 'test@example.com',
+       // ]);
     }
 }
